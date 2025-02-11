ad09fe7474aac5d44f8dabc941cf5230763aa342
Yejin Song <songyejin2000@gmail.com>
2025-02-11 15:59:32 +0900

    Feat: Springboot 프로젝트 초기화

---
+++ src/test/java/com/cantech/gateway/gateway_service/GatewayServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cantech.gateway.gateway_service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GatewayServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
