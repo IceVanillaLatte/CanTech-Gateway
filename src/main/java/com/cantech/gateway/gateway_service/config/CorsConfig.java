a55fd7626e2d5455d9dd955e189be79985d65616
yujinKim <yujinkimvv@gmail.com>
2025-03-01 14:42:26 +0900

    Fix: CORS 에러 해결 및 환경별 setAllowedOrigins 분리 (#3)

--- src/main/java/com/cantech/gateway/gateway_service/config/CorsConfig.java
+++ src/main/java/com/cantech/gateway/gateway_service/config/CorsConfig.java
@@ -1,6 +1,9 @@
 package com.cantech.gateway.gateway_service.config;
 
+import java.util.Arrays;
 import java.util.List;
+
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.cors.CorsConfiguration;
@@ -9,13 +12,18 @@ import org.springframework.web.cors.reactive.UrlBasedCorsConfigurationSource;
 
 @Configuration
 public class CorsConfig {
+
+    @Value("${cors.allowedOrigins}")
+    private String allowedOrigins;
+
     @Bean
     public CorsWebFilter corsWebFilter(){
         CorsConfiguration corsConfig = new CorsConfiguration();
-        corsConfig.setAllowedOrigins(List.of("*")); // 모든 도메인 허용
+        List<String> origins = Arrays.asList(allowedOrigins.split(","));
+        corsConfig.setAllowedOrigins(origins);
         corsConfig.setAllowedMethods(List.of("GET", "POST", "PUT", "DELETE"));
         corsConfig.setAllowedHeaders(List.of("*"));
-        corsConfig.setAllowCredentials(true);
+
 
         UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
         source.registerCorsConfiguration("/**", corsConfig);
